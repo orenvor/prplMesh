a5bc57d85201d2b7d9756fb9d17aa3f62115f430
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-07-21 10:59:01 +0300

    agent: slave: Remove deprecated variable
    Need to remove deprecated variable
    enable_keep_alive.
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- agent/src/beerocks/slave/son_slave_thread.h
+++ agent/src/beerocks/slave/son_slave_thread.h
@@ -40,7 +40,6 @@ public:
         uint16_t ucc_listener_port;
         std::string bridge_iface;
         int stop_on_failure_attempts;
-        bool enable_keep_alive;
         bool enable_repeater_mode;
         std::string backhaul_wire_iface;
         beerocks::eIfaceType backhaul_wire_iface_type;
